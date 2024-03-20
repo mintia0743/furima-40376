b88d155f1670eb62a6cdfb81509c55f581d713ae
mintia0743 <160447778+mintia0743@users.noreply.github.com>
2024-03-20 15:25:29 +0900

    テストコード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,12 +12,13 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true, exclusion: { in: [1] }
   validates :scheduled_delivery_id, presence: true, exclusion: { in: [1] }
   validates :price, presence: true,
-                    numericality: {
-                      only_integer: true,
-                      greater_than_or_equal_to: 300,
-                      less_than_or_equal_to: 9_999_999
-                    }
-  validates :price, format: { with: /\A[0-9]+\z/, message: "は半角数値のみ入力できます" }
+  numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999
+  },
+  format: { with: /\A[0-9]+\z/, message: "は半角数値のみ入力できます" }
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,47 +1,17 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Commerce.product_name }
-    description { Faker::Lorem.sentence }
-    image { Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "png") }
-    association :category, factory: :category
-    association :status, factory: :condition
-    association :shipping_fee, factory: :shipping_fee
-    association :prefecture, factory: :prefecture
-    association :scheduled_delivery, factory: :deliveryday
-    price { Faker::Number.between(from: 300, to: 99999999) }
-  end
-end
+    name {"オムライス"}
+    description{"説明を書きます"}
+    category_id { 2 }
+    status_id{ 2 }
+    shipping_fee_id{ 2 }
+    prefecture_id{ 2 }
+    scheduled_delivery_id{ 2 }
+    association :user
+    price {50000}
 
-FactoryBot.define do
-  factory :deliveryday, class: DeliveryDay do
-    id { 2 }
-    name { "1~2日で発送" }
-  end
-end
-FactoryBot.define do
-  factory :condition, class: Condition do
-    id { 2 }
-    name { "新品・未使用" }
-  end
-end
-
-FactoryBot.define do
-  factory :category, class: Category do
-    id { 2 }
-    name { "メンズ" }
-  end
-end
-
-FactoryBot.define do
-  factory :prefecture, class: Prefecture do
-    id { 2 }
-    name { "北海道" }
-  end
-end
-
-FactoryBot.define do
-  factory :shipping_fee, class: ShippingFee do
-    id { 2 }
-    name { "着払い(購入者負担)" }
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
