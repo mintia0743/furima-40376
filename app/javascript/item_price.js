40a77bed6d4ffe85ce9d224d179a9198cf134c92
mintia0743 <160447778+mintia0743@users.noreply.github.com>
2024-03-20 10:52:19 +0900

    モデルの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,14 @@
-const addTaxDom = document.getElementById("add-tax-price");
-const profitDom = document.getElementById("profit");
-const priceInput = document.getElementById("item-price");
+document.addEventListener("DOMContentLoaded", () => {
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+  const priceInput = document.getElementById("item-price");
 
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const tax = Math.floor(inputValue * 0.1);
-  const profit = inputValue - tax;
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const tax = Math.floor(inputValue * 0.1);
+    const profit = inputValue - tax;
 
-  addTaxDom.textContent = tax.toLocaleString();
-  profitDom.textContent = profit.toLocaleString();
+    addTaxDom.textContent = tax.toLocaleString();
+    profitDom.textContent = profit.toLocaleString();
+  });
 });
