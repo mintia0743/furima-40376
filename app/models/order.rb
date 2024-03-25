5ee66604279af03b14ab1d4978f0c96fcd0fe93b
mintia0743 <160447778+mintia0743@users.noreply.github.com>
2024-03-25 17:13:52 +0900

    モデルの作成

---
+++ app/models/order.rb
@@ -0,0 +1,11 @@
+class Order < ApplicationRecord
+  class Order < ApplicationRecord
+    belongs_to :user
+    belongs_to :item
+    has_one :address
+  
+    validates :user, presence: true
+    validates :item, presence: true
+  end
+  
+end
