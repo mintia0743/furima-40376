5cd6ef82e0d8f6f4e73491ea484147c89b206ac3
mintia0743 <160447778+mintia0743@users.noreply.github.com>
2024-03-27 16:58:30 +0900

    修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   before do
     user = FactoryBot.create(:user)
-    item = FactoryBot.create(:item, user_id: user.id)
+    item = FactoryBot.create(:item)
     @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
 end
   describe 'validations' do
@@ -51,18 +51,20 @@ end
         expect(@order_address.errors[:phone_number]).to include("は10桁以上11桁以内の半角数値のみ入力してください")
       end
 
-      it 'validates format of phone_number' do
-        # 電話番号が9桁以下の場合
+      it '電話番号が９桁以下では保存できない' do
+        
         @order_address.phone_number = '123456789'
         @order_address.valid?
         expect(@order_address.errors[:phone_number]).to include("は10桁以上11桁以内の半角数値のみ入力してください")
-      
-        # 電話番号が12桁以上の場合
+      end
+
+        it '電話番号が１２桁以上では保存できない' do
         @order_address.phone_number = '123456789012'
         @order_address.valid?
         expect(@order_address.errors[:phone_number]).to include("は10桁以上11桁以内の半角数値のみ入力してください")
-      
-        # 電話番号に半角数字以外が含まれている場合
+        end
+
+        it '電話番号に半角数字以外が含まれているときは保存できない' do
         @order_address.phone_number = '090-1111-1111'
         @order_address.valid?
         expect(@order_address.errors[:phone_number]).to include("は10桁以上11桁以内の半角数値のみ入力してください")
